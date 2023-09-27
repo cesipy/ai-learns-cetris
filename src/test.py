8fe3816334156b3eeb4bc589c94055047c98ff48
cedric <cedric.sillaber@gmail.com>
2023-09-27 20:50:21 +0200

    update tetris and other changes

--- src/test.py
+++ src/test.py
@@ -1,7 +1,22 @@
-import subprocess
+import subprocess as sub
+import os
+
+
+def child():
+    print("hello from child")
+
 
 def main():
-    subprocess.run("./tetris", shell=True)
+    pid = os.fork()
 
+    if pid == 0:
+        # child process to handle the tetris game
+        child()
+
+    else:
+        # parent
+        tetris_command = './tetris_implementation/tetris'
+
+        sub.run(tetris_command, shell=True)
 
 main()
