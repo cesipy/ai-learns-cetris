288c6f2f0d38c52c1f4264aba614daaf46e7740e
cedric <cedric.sillaber@gmail.com>
2023-10-01 20:11:46 +0200

    proper communication between c and python

--- src/main.py
+++ src/main.py
@@ -13,14 +13,27 @@ def parse_control(relative_position_change: int, should_rotate: bool, ):
     pass
 
 
-def child():
-    # with open(FIFO_STATES) as f:
-    #    while True:
-    #        data = f.read()
-    #        if len(data) != 0:
-    #            print("read: {}".format(data))
-    #        else:
-    #            break
+def receive_from_pipe():
+    try:
+        # Open the FIFO for reading
+        fifo_fd = os.open(FIFO_STATES, os.O_RDONLY)
+
+        # Read data from the FIFO
+        data = os.read(fifo_fd, 100)  # Adjust the buffer size as needed
+
+        # Close the FIFO
+        os.close(fifo_fd)
+
+        return data.decode('utf-8')  # Assuming data is in UTF-8 encoding
+    except FileNotFoundError:
+        print(f"Error: {FIFO_STATES} does not exist.")
+        return ""
+    except Exception as e:
+        print(f"Error while reading from {FIFO_STATES}: {e}")
+        return ""
+
+
+def sent_to_pipe():
     with open(FIFO_CONTROLS, "w") as f:
         data = random.randint(-6, 6)
         f.write(str(data))
@@ -32,11 +45,16 @@ def main():
     if pid == 0:
         # child process to handle the tetris game
         time.sleep(1)
-        for _ in range(100):
-            child()
-            time.sleep(350 / 1000)
+        data: str = ""
+        while not data == "end":
 
-        print("done through iterations")
+            receive_from_pipe()
+
+            time.sleep(350/1000)
+
+            sent_to_pipe()
+
+        print("reached!")
         exit(0)
     else:
         # parent
