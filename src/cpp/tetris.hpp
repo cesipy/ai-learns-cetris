21baf8ac036fff4be59d74ed761e24c5095d3047
Cedric Sillaber <cedric.sillaber@gmail.com>
2023-11-12 15:31:33 +0100

    refactor file structure

--- src/cpp/tetris.hpp
+++ src/cpp/tetris.hpp
@@ -289,27 +289,7 @@ const int setup_named_pipe(const char* name, mode_t permission, int mode );
 
 /* ----------------------------------------------------------- */
 
-// logger implementation
-inline std::string getCurrentDateTime( std::string s )
-{
-    time_t now = time(0);
-    struct tm  tstruct;
-    char  buf[80];
-    tstruct = *localtime(&now);
-    if(s=="now")
-        strftime(buf, sizeof(buf), "%Y-%m-%d %X", &tstruct);
-    else if(s=="date")
-        strftime(buf, sizeof(buf), "%Y-%m-%d", &tstruct);
-    return std::string(buf);
-};
-
-inline void Logger( std::string logMsg )
-{
-    std::string filePath = "../logs/log_"+getCurrentDateTime("date")+".txt";
-    std::string now = getCurrentDateTime("now");
-    std:: ofstream ofs(filePath.c_str(), std::ios_base::out | std::ios_base::app );
-    ofs << now << '\t' << logMsg << '\n';
-    ofs.close();
-}
+std::string getCurrentDateTime( std::string s );
+void Logger( std::string logMsg );
 
 #endif
