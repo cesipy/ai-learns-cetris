2c8546ee372101e1c48cdaac17472b6ede932505
Cedric Sillaber <cedric.sillaber@gmail.com>
2023-12-05 10:30:59 +0100

    refactor logger

--- src/simpleLogger.py
+++ src/simpleLogger.py
@@ -1,10 +1,11 @@
 import datetime
 
+FILE_NAME = "../logs/py_log_"
 
 class SimpleLogger:
     def __init__(self ):
         today = datetime.datetime.today().strftime("%Y-%m-%d")
-        self.filename = "../logs/py_log_" + today + ".txt"
+        self.filename = FILE_NAME + today + ".txt"
 
     def log(self, message: str) -> None:
         timestamp = datetime.datetime.now().strftime("%H:%M:%S")
