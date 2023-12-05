46019d6e1ac76dcf102ee9042cdbf56799cb4e5c
Cedric Sillaber <cedric.sillaber@gmail.com>
2023-12-05 10:28:48 +0100

    init kera model & tests

--- src/q_agent.py
+++ src/q_agent.py
@@ -2,31 +2,89 @@ import keras
 import numpy as np
 import random
 
+MODEL_NAME = "model.h5"
+
 class Agent:
-    def __init__(self, number_layers, epsilon, q_table):
-        self.number_layers = number_layers
-        self.epsilon       = epsilon
-        self.q_table       = q_table
-        self.model         = self.init_model()
+    def __init__(self, n_neurons, epsilon, q_table, load_model: bool = False):
+        self.n_neurons = n_neurons
+        self.epsilon = epsilon
+        self.q_table = q_table
+
+        if load_model:
+            # load keras model from file
+            self.model = self.load_model()
+
+        else:
+            # build a new keras sequential model
+            self.model = self.init_model()
 
 
     def epsilon_greedy_policy(self, state):
         if random.random() < self.epsilon:
-            pass
-            # return random state 
+            # Explore: choose a random action
+            return random.choice(["rotate", "left", "right"])
         else:
-            # return best q value for state
-            pass
+            # Exploit: choose the action with the highest Q-value
+            q_values = self.predict(state)
+            return max(q_values, key=q_values.get)
 
-
-    def predict(self, state): 
-        pass
+    def predict(self, state):
+        # Assuming state is a list of features [holes, lines_cleared, bumpiness, piece_type, height]
+        q_values = self.model.predict(np.array([state]))[0]
+        return {action: q_values[i] for i, action in enumerate(["rotate", "left", "right"])}
 
 
     def init_model(self):
+        # temp: magic numbers
+        n_output = 3  # rotate, left, right
+        input_shape = [5]  # holes, lines cleared, bumpiness, piece_type, height
+
         model = keras.models.Sequential()
-        model.add(keras.layers.Dense(self.number_layers, activation="relu", input_shape=(5,)))
-        model.add(keras.layers.Dense(units=2, activation='linear'))  # Assuming 2 actions
+
+        # input layer with 5 nodes
+        model.add(keras.layers.Dense(input_shape[0], activation="relu", input_shape=input_shape))
+        model.add(keras.layers.Dense(self.n_neurons, activation="relu"))  # one hidden layer
+        model.add(keras.layers.Dense(n_output))  # for output (rotate, left, right)
+
+        model.summary()
         model.compile(optimizer='adam', loss='mean_squared_error')
-        
+
         return model
+
+
+    def save_model(self):
+        """
+        saves keras model as a file.
+        """
+        self.model.save(MODEL_NAME)
+
+
+    def load_model(self):
+        """
+        loads a saved keras model.
+        """
+        return keras.models.load_model(MODEL_NAME)
+
+
+def testing():
+    n_neurons = 30
+    epsilon = 0.3
+    q_table = {}
+
+    agent = Agent(n_neurons, epsilon, q_table)
+
+    # temporary tests
+    state = [1, 2, 3, 4, 5]  
+    action = agent.epsilon_greedy_policy(state)
+    print(f"Selected action: {action}")
+
+    q_values = agent.predict(state)
+    print(f"Q-values: {q_values}")
+
+
+def main():
+    testing()
+
+
+if __name__ == '__main__':
+    main()
