11192ec01bf253653bfb5fde6adf46442c566b53
Cedric Sillaber <cedric.sillaber@gmail.com>
2023-12-10 18:40:00 +0100

    tests for logger

--- tests/logger_tests.py
+++ tests/logger_tests.py
@@ -1,5 +1,7 @@
 import time
 import os, sys
+import pytest
+import datetime
 
 # to import packages from src
 # https://stackoverflow.com/questions/1896918/running-unittest-with-typical-test-directory-structure
@@ -19,4 +21,34 @@ def logger_test(n: int):
         logger.log(f"{i}'s log message")
 
 
-logger_test(100)
+
+def test_log_message_is_written():
+    logger = SimpleLogger()
+
+    message = "Test log message"
+    logger.log(message)
+
+    with open(logger.filename, "r") as f:
+        contents = f.read()
+        assert message in contents
+
+
+def test_log_format():
+    logger = SimpleLogger()
+
+    message = "Test log message"
+    logger.log(message)
+
+    expected_entry = f"{datetime.datetime.now().strftime('%H:%M:%S')}- {message}\n"
+    with open(logger.filename, "r") as f:
+        contents = f.read()
+        assert expected_entry in contents
+
+
+@pytest.fixture(autouse=True)
+def cleanup_logs(request):
+    yield
+    # remove the log files created during the tests
+    for filename in os.listdir("../logs/"):
+        if filename.startswith("py_log_"):
+            os.remove(os.path.join("../logs/", filename))
