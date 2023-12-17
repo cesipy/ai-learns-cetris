6829480041b43533b1e2d43a9b82dd8d0e41d88d
Cedric Sillaber <cedric.sillaber@gmail.com>
2023-12-17 13:18:19 +0100

    game class, to save information about training

--- src/metadata.py
+++ src/metadata.py
@@ -1,5 +1,7 @@
 import numpy as np
+import pickle
 
+COUNTER_THRESH = 20
 
 class Metadata:
     def __init__(self, logger, fifo_states_name, fifo_controls_name, fd_states:int, fd_controls:int):
@@ -9,12 +11,12 @@ class Metadata:
         self.fd_states = fd_states
         self.fd_controls = fd_controls
         
+
     def debug(self):
         return "fd_states: " + str(self.fd_states) + " fd_controls: " + str(self.fd_controls)
     
 
 
-    
 class State:
     def __init__(self, lines_cleared, height, holes, bumpiness, piece_type):
         self.lines_cleared = lines_cleared
@@ -47,3 +49,58 @@ class State:
         piece_type = self.piece_type
 
         return lines_cleared, height, holes, bumpiness, piece_type
+    
+
+
+class Game:
+    def __init__(self):
+        self.lines_cleared = 0
+        self.epoch         = 0
+        self.counter       = 0
+        self.epsilon       = 0
+
+    def increase_epoch(self):
+        self.epoch += 1
+
+    def set_lines_cleared(self, lines_cleared):
+        self.lines_cleared = lines_cleared
+
+    def increase_counter(self):
+        """
+        returns true, if counter reaches threshold, defined in `COUNTER_THRESH`
+        """
+    
+    def set_epoch(self, epoch):
+        self.epoch = epoch
+
+    def set_epsilon(self, epsilon):
+        self.epsilon = epsilon
+
+    def load_model(self):
+        file_path = "../res/saved_game.pkl"
+        with open(file_path, 'rb') as f:
+            obj:Game = pickle.load(f)
+        
+        self.set_lines_cleared(obj.lines_cleared)
+        self.set_epoch(obj.epoch)
+        
+
+    def save_model(self):
+        file_path = "../res/saved_game.pkl"
+        with open(file_path, 'wb') as f:
+            pickle.dump(self, f)
+
+    def __repr__(self):
+        string:str = f"""
+    ----------------------------------------------------------------------
+
+    ----------------------------------------------------------------------
+    current epoch        ={self.epoch}
+    current lines cleared={self.lines_cleared}
+    current epsilon      ={self.epsilon}
+    ----------------------------------------------------------------------
+
+    ----------------------------------------------------------------------
+
+    """
+        return string
